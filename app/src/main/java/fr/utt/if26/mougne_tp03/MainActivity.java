5d04a6c8a797102a1abd43ef2308aeedd7e32e9a
mougnead <adrian.mougne@utt.fr>
2017-10-07 16:40:49 +0200

    Ajout d'une 2 eme activité (lOgin) + lien entre activité

--- app/src/main/java/fr/utt/if26/mougne_tp03/MainActivity.java
+++ app/src/main/java/fr/utt/if26/mougne_tp03/MainActivity.java
@@ -1,18 +1,22 @@
 package fr.utt.if26.mougne_tp03;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView msg = null;
+    Button btn = null;
 
     /**
      * Kikou
      * @author Adrian
-     * @see textview
-     * @param savedInstanceState
+      * @param savedInstanceState
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         msg = (TextView) findViewById(R.id.message);
-      //  msg.setText("Message androidas");
+         msg.setText("Message androidas");
+
+    }
+
+    public void sendMessage(View view) {
+        Log.d("ee","dd");
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
